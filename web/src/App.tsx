82876929aaea4b148119fb5d16be2255f54bb46f
Lee Zhen Jie <finalight@gmail.com>
2022-09-13 16:11:25 +0800

    set graphql context correctly

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import { ApolloLink, HttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
@@ -8,8 +9,20 @@ import Routes from 'src/Routes'
 
 import './index.css'
 
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  // return the headers to the context so httpLink can read them
+  const token = localStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : null,
+    },
+  }
+})
+
 const httpLink = new HttpLink({ uri: `${process.env.GRAPHQL_URL}` })
-const link: ApolloLink = [httpLink]
+const link: ApolloLink = authLink.concat(httpLink)
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
